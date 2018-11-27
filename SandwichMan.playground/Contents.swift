ccdf7a2e5f39560b88c8e7a2de13af3dedfca7bf
nwainwri <nathanwainwright@gmail.com>
2018-11-27 12:58:33 -0800

    improved hasAtLeastOneIngredient function to have better flow, and validateSandwich function to handle an array of strings and ints

--- SandwichMan.playground/Contents.swift
+++ SandwichMan.playground/Contents.swift
@@ -6,7 +6,7 @@ import UIKit
 //  * For each extra ingredient, there is an additional charge of $0.50
 
 // == Ingredients ==============
-let proteins   = [ "chicken", "meatballs", "sardines", "turkey", "salmon", "bacon" ,  "soy something"  ]
+let proteins   = [ "chicken", "meatballs", "sardines", "turkey", "salmon", "bacon" , "soy something" ]
 let veggies    = [ "lettuce", "onion", "cucumber", "sprouts", "carrot" ]
 let fruits     = [ "tomato" ]
 let condiments = [ "mayo", "mustard", "relish", "pickles", "ketchup" ]
@@ -14,31 +14,26 @@ let condiments = [ "mayo", "mustard", "relish", "pickles", "ketchup" ]
 // == Question 1 ===============
 // A sandwich must contain at least 1 ingredient. Write a function called `hasAtLeastOneIngredient` that accepts an array of ingredients as an argument, and returns a boolean. If one or more ingredients are present, it should return true
 //  i.e. An input of [ 'chicken', 'mustard', 'pickles'] would return true
-let chickenSandwich = ["chicken", "mustard", "pickles"]
 
-func hasAtLeastOneIngredient(_ ingredients:[String]) -> Bool {
+let chickenSandwich = [ "chicken", "mustard", "pickles" ]
+
+func hasAtLeastOneIngredient(_ ingredients: [String]) -> Bool {
   var hasOneIngredient = false
   
-  if (hasOneIngredient == false) && (checkForProteins(ingredients) == true) {
+  if checkForProteins(ingredients) {
     hasOneIngredient = true
-  }
-  
-  if (hasOneIngredient == false) && (checkForVeggies(ingredients) == true) {
+  } else if checkForVeggies(ingredients) {
     hasOneIngredient = true
-  }
-  
-  if (hasOneIngredient == false) && (checkForFruits(ingredients) == true) {
+  } else if checkForFruits(ingredients){
     hasOneIngredient = true
-  }
-  
-  if (hasOneIngredient == false) && (checkForCondiments(ingredients) == true) {
+  } else if checkForCondiments(ingredients) {
     hasOneIngredient = true
-  }
+  } else {}
   return hasOneIngredient
 }
 
-func checkForProteins(_ ingredients:[String]) -> Bool {
-  var hasProtein:Bool = false
+func checkForProteins(_ ingredients: [String]) -> Bool {
+  var hasProtein: Bool = false
   for ingredient in ingredients {
     if proteins.contains(ingredient) {
       hasProtein = true
@@ -47,8 +42,8 @@ func checkForProteins(_ ingredients:[String]) -> Bool {
   return hasProtein
 }
 
-func checkForVeggies(_ ingredients:[String]) -> Bool {
-  var hasVeggie:Bool = false
+func checkForVeggies(_ ingredients: [String]) -> Bool {
+  var hasVeggie: Bool = false
   for ingredient in ingredients {
     if veggies.contains(ingredient) {
       hasVeggie = true
@@ -57,8 +52,8 @@ func checkForVeggies(_ ingredients:[String]) -> Bool {
   return hasVeggie
 }
 
-func checkForFruits(_ ingredients:[String]) -> Bool {
-  var hasFruit:Bool = false
+func checkForFruits(_ ingredients: [String]) -> Bool {
+  var hasFruit: Bool = false
   for ingredient in ingredients {
     if fruits.contains(ingredient) {
       hasFruit = true
@@ -67,8 +62,8 @@ func checkForFruits(_ ingredients:[String]) -> Bool {
   return hasFruit
 }
 
-func checkForCondiments(_ ingredients:[String]) -> Bool {
-  var hasCondiment:Bool = false
+func checkForCondiments(_ ingredients: [String]) -> Bool {
+  var hasCondiment:  Bool = false
   for ingredient in ingredients {
     if condiments.contains(ingredient) {
       hasCondiment = true
@@ -78,22 +73,28 @@ func checkForCondiments(_ ingredients:[String]) -> Bool {
 }
 
 hasAtLeastOneIngredient(chickenSandwich)
-//checkForProteins(chickenSandwich)
-//checkForVeggies(chickenSandwich)
-//checkForFruits(chickenSandwich)
-//checkForCondiments(chickenSandwich)
-
 
 // == Question 2 ===============
 // A sandwich can only contain ingredients on the menu; if a sandwich contains an ingredient not on the menu, it should be removed. Write a function called `validateSandwich` that accepts an array of ingredients as an argument and returns only the valid ingredients as an array
 //  i.e. An input of [ 'chicken', 'hammer', 36, 'swiss cheese' ] would return [ 'chicken' ]
 
-let firstSandwichOrder = ["chicken", "hammer", "36", "swiss cheese"]
+let firstSandwichOrder = [ "chicken", "hammer", 36, "swiss cheese" ] as [Any]
 
-func validateSandwich(_ ingredients: [String]) -> [String] {
-  var validatedIngredients:[String] = []
+func validateSandwich(_ ingredients: [Any]) -> [String] {
+  var convertedIngredients: [String] = []
+  
+  for object in ingredients {
+    if let stringIngredient = object as? String {
+      convertedIngredients.append(stringIngredient)
+    } else if let numberIngredient = object as? Int {
+      convertedIngredients.append(String(numberIngredient))
+    }
+    else {}
+  }
+  
+  var validatedIngredients: [String] = []
 
-  for ingredient in ingredients {
+  for ingredient in convertedIngredients {
     
     if proteins.contains(ingredient) {
       validatedIngredients.append(ingredient)
@@ -123,9 +124,9 @@ validateSandwich(firstSandwichOrder)
 // Write a function that calculates the number of extra ingredients in a sandwich. It accepts an array of ingredients as an argument and returns the number of extra ingredients as an integer. Name the function whatever you like.
 //  i.e. A sandwich containing ['turkey', 'lettuce', 'mayo', 'mayo', 'mayo' ] would return 1
 
-let secondSandwichOrder = ["turkey", "lettuce", "mayo", "mayo", "mayo"]
+let secondSandwichOrder = [ "turkey", "lettuce", "mayo", "mayo", "mayo" ]
 
-func checkForExtraIngredients(_ ingredients:[String]) -> Int {
+func checkForExtraIngredients(_ ingredients: [String]) -> Int {
   var hasHowManyExtraIngredients: Int = 0
   var countedIngredients: [String: Int] = [:]
   
@@ -133,7 +134,7 @@ func checkForExtraIngredients(_ ingredients:[String]) -> Int {
     countedIngredients[ingredient] = (countedIngredients[ingredient] ?? 0) + 1
   }
   
-  for (ingredient, amount) in countedIngredients {
+  for (_, amount) in countedIngredients {
     if amount > 1 {
       hasHowManyExtraIngredients += 1
     }
@@ -149,7 +150,6 @@ checkForExtraIngredients(secondSandwichOrder)
 //  * A standard sandwich will cost $12 (…you're located in Gastown)
 //  * A standard sandwich has up to 1 protein, up to 3 fruits or veggies, and up to 2 condiments
 //  * For each extra ingredient, there is an additional charge of $0.50
-
 
 func howMuchDoesSandwichCost(proteinsCount: Int, fruitsAndVeggiesCount: Int, condimentsCount: Int) -> Double {
   let kSandwichCost: Double = 12.00
@@ -182,11 +182,8 @@ howMuchDoesSandwichCost(proteinsCount: 1, fruitsAndVeggiesCount: 3, condimentsCo
 // You've decided to increase the price of your add-ons. The new costs are: $1.50 for each extra protein, $0.75 for fruit/veggies and $0.50 for condiments. Write a function that calculates the cost of a sandwich, reflecting this price change. The function still accepts 3 integers as arguments and returns the total cost as a double
 //  i.e. An input of 2 (protein) and 1 (fruits/veggies) would return 13.50
 
-
-
 func howMuchDoesSandwichCostWithIncrease(proteinsCount: Int, fruitsAndVeggiesCount: Int, condimentsCount: Int) -> Double {
   let kSandwichCost: Double = 12.00
-
   let kExtraProteinCost: Double = 1.50
   let kExtraFruitsAndVeggiesCost: Double = 0.75
   let kExtraCondimentsCost: Double = 0.50
